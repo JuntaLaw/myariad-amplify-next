9ebd1b14f27dd3ea855d07e8a094953f172f3e55
JuntaLaw <junta_law@me.com>
2024-03-30 22:25:30 +0900

    Toppage update (redirect to home)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,43 @@
 // src/app/page.js
+// "use client";
+// import React from 'react'; 
+// import { CustomAuthenticator } from "../ui-components/authenticator.override"; 
+// import Home from "./home/page";
+
+// export default function App() {
+//   return (
+//     <div className="authenticator-wrapper">
+//       <CustomAuthenticator>
+//         <main>  
+//           {/* ログイン後に表示するコンテンツをここに追加 */}
+//           <Home/>
+//         </main> 
+//       </CustomAuthenticator>
+//     </div>
+//   );
+// }
+
+// src/app/page.js
 "use client";
-import React from 'react'; 
-import { CustomAuthenticator } from "../ui-components/authenticator.override"; 
-// import Toppage from './toppage/page';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import Home from "./home/page";
 
 export default function App() {
+  const router = useRouter();
+
+  // 一時的なリダイレクト処理
+  React.useEffect(() => {
+    router.replace('/home');
+  }, []);
+
   return (
     <div className="authenticator-wrapper">
-      <CustomAuthenticator>
-        <main>  
-          {/* ログイン後に表示するコンテンツをここに追加 */}
-          {/* <Toppage/> */}
-        </main> 
-      </CustomAuthenticator>
+      {/* 認証コンポーネントを一時的に削除 */}
+      <main>
+        {/* ログイン後に表示するコンテンツをここに追加 */}
+        <Home />
+      </main>
     </div>
   );
 }
