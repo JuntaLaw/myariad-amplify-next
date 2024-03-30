a507b4c8946848f9ef28beee4fc14b0102abbadd
JuntaLaw <junta_law@me.com>
2024-03-30 22:36:32 +0900

    toppage update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,43 +1,17 @@
-// src/app/page.js
-// "use client";
-// import React from 'react'; 
+import React from 'react'; 
 // import { CustomAuthenticator } from "../ui-components/authenticator.override"; 
-// import Home from "./home/page";
-
-// export default function App() {
-//   return (
-//     <div className="authenticator-wrapper">
-//       <CustomAuthenticator>
-//         <main>  
-//           {/* ログイン後に表示するコンテンツをここに追加 */}
-//           <Home/>
-//         </main> 
-//       </CustomAuthenticator>
-//     </div>
-//   );
-// }
-
-// src/app/page.js
-"use client";
-import React from 'react';
-import { useRouter } from 'next/navigation';
 import Home from "./home/page";
 
 export default function App() {
-  const router = useRouter();
-
-  // 一時的なリダイレクト処理
-  React.useEffect(() => {
-    router.replace('/home');
-  }, []);
-
   return (
-    <div className="authenticator-wrapper">
-      {/* 認証コンポーネントを一時的に削除 */}
-      <main>
-        {/* ログイン後に表示するコンテンツをここに追加 */}
-        <Home />
-      </main>
-    </div>
+    // <div className="authenticator-wrapper">
+      // <CustomAuthenticator>
+        <main>  
+          {/* ログイン後に表示するコンテンツをここに追加 */}
+          <Home/>
+        </main> 
+      // </CustomAuthenticator>
+    // </div>
   );
 }
+
