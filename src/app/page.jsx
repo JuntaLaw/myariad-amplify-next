e857302bf840d0b67c061ad187a0440556a07880
JuntaLaw <junta_law@me.com>
2024-03-30 22:47:31 +0900

    top　revise

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,17 @@
 import React from 'react'; 
-// import { CustomAuthenticator } from "../ui-components/authenticator.override"; 
+import { CustomAuthenticator } from "../ui-components/authenticator.override"; 
 import Home from "./home/page";
 
 export default function App() {
   return (
-    // <div className="authenticator-wrapper">
-      // <CustomAuthenticator>
+    <div className="authenticator-wrapper">
+      <CustomAuthenticator>
         <main>  
           {/* ログイン後に表示するコンテンツをここに追加 */}
-          <Home/>
+          {/* <Home/> */}
         </main> 
-      // </CustomAuthenticator>
-    // </div>
+      </CustomAuthenticator>
+    </div>
   );
 }
 
